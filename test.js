5ea8abaaa34dc3426919ae2cb5b11b445633c2f3
GitOffMyCode <52583811+GitOffMyCode@users.noreply.github.com>
2023-11-30 20:13:27 +0000

    updated

---
+++ test.js
@@ -0,0 +1,5 @@
+let butterflies = ["peacock", "painted lady", "orange tip", "speckled wood"];
+// for loop
+for (i = 0; i < butterflies.length; i++) {
+  console.log(`I just saw a lovely`, butterflies[i]);
+}
